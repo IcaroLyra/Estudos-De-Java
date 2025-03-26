d3fe5291976ca74ebc55a689bee803d93a8d749b
Icaro <icraolyra@gmail.com>
2025-03-25 21:53:08 -0300

    Renomeia variável de cachorro para gato
    Alterado o nome da variável `nomeDoMeuCachorro` para `nomeDoMeuGato`
    Atualizada a impressão no console para refletir a mudança

--- curso-java-basico/src/com/icaro/cursojava/aula10/Variaveis.java
+++ curso-java-basico/src/com/icaro/cursojava/aula10/Variaveis.java
@@ -4,13 +4,14 @@ public class Variaveis {
   public static void main(String[] args) {
     int idade = 19;
     String nome = "icaro";
-    String nomeDoMeuCachorro = "toto";
+    String nomeDoMeuGato = "safira";
 
     idade = 20;
 
     System.out.println("Idade = " + idade);
     System.out.println("Nome = " + nome);
-    System.out.println("Nome do cachorro = " + nomeDoMeuCachorro);
+    System.out.println("Nome do cachorro = " + nomeDoMeuGato);
+    
 
   }
 }
