294b7146fea9a745eda1c2982fcd1adc3d005e31
Icaro <icraolyra@gmail.com>
2025-04-07 08:32:44 -0300

    Adiciona exemplos de laços while e do-while
    Foram criadas duas classes (LoopWhile e LoopDoWhile) para exemplificar o uso dos laços while e do-while em Java.
    
    Esses arquivos fazem parte da aula 16 e têm como objetivo demonstrar o comportamento de repetição com controle baseado em condição, evidenciando a diferença entre executar a verificação antes (while) e depois (do-while) do bloco de código.

---
+++ curso-java-basico/src/com/icaro/cursojava/aula16/LoopDoWhile.java
@@ -0,0 +1,17 @@
+package com.icaro.cursojava.aula16;
+
+public class LoopDoWhile {
+  public static void main(String[] args) {
+
+    int i = 1;
+
+    do {
+      i++;
+      System.out.println("Valor de i: " + i);
+
+    } while (i < 13);
+    System.out.println(i);
+
+  }
+
+}
