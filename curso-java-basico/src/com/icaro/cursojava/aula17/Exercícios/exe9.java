f356fa12e0eafbac20ca19b6bbba8a1206b4b17c
Icaro <icraolyra@gmail.com>
2025-04-08 08:34:10 -0300

    ✨ feat: imprime números ímpares até 50
    Usa laço de repetição com incremento de 2 para exibir apenas números ímpares entre 1 e 50.
    Remove uso desnecessário de Scanner.
    
    closes #9

---
+++ curso-java-basico/src/com/icaro/cursojava/aula17/Exercícios/exe9.java
@@ -0,0 +1,19 @@
+package com.icaro.cursojava.aula17.Exercícios;
+
+import java.util.Scanner;
+
+public class exe9 {
+  public static void main(String[] args) {
+
+    int i = 1;
+    int max = 50;
+
+    while (i <= max) {
+      System.out.println(i);
+      i = i + 2;
+
+    }
+
+  }
+
+}
