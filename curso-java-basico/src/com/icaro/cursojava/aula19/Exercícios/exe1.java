ea73957542b6d765cd486cd8426ae471122ec903
Icaro <icraolyra@gmail.com>
2025-04-10 08:19:26 -0300

    ✨ feat: implementa cópia de vetor
    Adiciona lógica para copiar os elementos do vetor A para o vetor B,
    demonstrando manipulação básica de arrays em Java com laços `for`.
    
    Inclui impressão dos dois vetores no console para visualização do resultado.

---
+++ curso-java-basico/src/com/icaro/cursojava/aula19/Exercícios/exe1.java
@@ -0,0 +1,30 @@
+package com.icaro.cursojava.aula19.Exercícios;
+
+public class exe1 {
+  public static void main(String[] args) {
+
+    int[] A = new int[5];
+    int[] B = new int[5];
+
+    A[0] = 10;
+    A[1] = 20;
+    A[2] = 30;
+    A[3] = 40;
+    A[4] = 50;
+
+    for (int i = 0; i < A.length; i++) {
+      B[i] = A[i];
+    }
+
+    System.out.println("Vetor A:");
+    for (int i = 0; i < A.length; i++) {
+      System.out.println("A[" + i + "] = " + A[i]);
+    }
+
+    System.out.println("\nVetor B:");
+    for (int i = 0; i < B.length; i++) {
+      System.out.println("B[" + i + "] = " + B[i]);
+    }
+
+  }
+}
