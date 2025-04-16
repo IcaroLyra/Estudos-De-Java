775c97e3bf068f63d21ade47e696a68126b2aedb
Icaro <icraolyra@gmail.com>
2025-04-16 19:34:38 -0300

    ✨ feat: adicionar classe Pacotes com imports
    Cria o pacote com.icaro.cursojava.aula28 com a classe Pacotes, que demonstra a utilização de imports para acessar
    Carro (aula 27) e exe1 (aula 20 → Exercícios). A classe serve como exemplo prático de organização de código em múltiplos pacotes.

---
+++ curso-java-basico/src/com/icaro/cursojava/aula28/Pacotes.java
@@ -0,0 +1,12 @@
+package com.icaro.cursojava.aula28;
+
+
+import com.icaro.cursojava.aula20.Exercícios.*;
+import com.icaro.cursojava.aula27.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        Carro carro;
+        exe1 exe1;
+    }
+}
