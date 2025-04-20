e84e5b92c45936c43ce6af0aff675dceea62bae6
Icaro <icraolyra@gmail.com>
2025-04-19 23:38:03 -0300

    🎉 feat(aula32): adicionar classe Carro com encapsulamento e teste inicial
    Implementa a classe `Carro` com atributos privados e métodos de acesso (getters e setters).
    Também adiciona a classe `TesteCarro` para validar a criação do objeto e chamada dos métodos.
    
    Permite testes iniciais de encapsulamento conforme conteúdo da aula 32.
    
    Refs: curso Java básico

---
+++ curso-java-basico/src/com/icaro/cursojava/aula30/Teste.java
@@ -0,0 +1,38 @@
+package com.icaro.cursojava.aula30;
+
+public class Teste {
+
+    String nome;
+    int idade;
+    double altura;
+
+    public Teste(String nome, double altura, int idade) {
+        this.nome = nome;
+        this.altura = altura;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
--- curso-java-basico/src/com/icaro/cursojava/aula30/TesteCarro.java
+++ curso-java-basico/src/com/icaro/cursojava/aula30/TesteCarro.java
@@ -6,5 +6,6 @@ public class TesteCarro {
         Carro van = new Carro("Fiat", "Ducato");
 
         System.out.println(van.numPassageiros);
+
     }
 }
