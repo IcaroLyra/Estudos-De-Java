caf91b14171c3274187f538cc29c40d71a9021b4
Icaro <icraolyra@gmail.com>
2025-05-02 11:18:23 -0300

    🎉 feat(model): implementar herança entre Pessoa, Aluno e Professor
    Refatoradas as classes Aluno e Professor para herdar atributos e comportamentos de Pessoa,
    centralizando dados comuns (nome, telefone, CPF, etc.) e promovendo reuso com herança.
    
    Refs: aula37

---
+++ curso-java-basico/src/com/icaro/cursojava/aula37/Teste.java
@@ -0,0 +1,12 @@
+package com.icaro.cursojava.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+
+        aluno.setNome("Maria");
+
+    }
+}
